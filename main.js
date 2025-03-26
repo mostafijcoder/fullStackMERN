55fbd5c0b6c66884ff8d7938c27fe932e9f1be68
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-26 13:10:49 +0600

    MVC structure router callback function to homecontroller.js

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
+const homeController = require("./controllers/homeControllers");
 const port=3011;
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+    app.use(
+    express.urlencoded({
+    extended: false
+    })
+   ); 
+   app.use(express.json());
+   
+    app.use((req, res, next) => {
     console.log(`request made to: ${req.url}`);
     next();
    });
-   
-app.get('/items/:vegetable', (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`This is the page for ${veg}`);
+   app.post("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send("POST Successful!");
+   });
+   // get the posted data
+    app.get("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
+    res.send("GET Successful!");
     }
-)
-.listen(port, () => {
+    );
+
+    app.get("/items/:vegetable", homeController.sendReqParam);
+    app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     }
 );
