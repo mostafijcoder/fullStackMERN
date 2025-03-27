27ace7674f96958baa3dd402b2d633beabe353b9
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-27 13:10:19 +0600

    rendering a view index.ejs template from a controller action by passing data

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,4 +1,9 @@
 exports.sendReqParam = (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`This is the page for ${veg}`);
-   };
+    let vegetable = req.params.vegetable;
+    res.send(`This is the page for ${vegetable}`);
+};
+// Rendering a view from a controller action in homeController.js
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName;
+    res.render("index", { name: paramsName });
+}
--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-const homeController = require("./controllers/homeControllers");
-const port=3011;
-const express = require('express');
+const express = require("express");
+const homeController = require("./controllers/homeControllers"); // Corrected file name
 const app = express();
 
-    app.use(
-    express.urlencoded({
-    extended: false
-    })
-   ); 
-   app.use(express.json());
-   
-    app.use((req, res, next) => {
-    console.log(`request made to: ${req.url}`);
-    next();
-   });
-   app.post("/", (req, res) => {
-    console.log(req.body);
-    console.log(req.query);
-    res.send("POST Successful!");
-   });
-   // get the posted data
-    app.get("/", (req, res) => {
-    console.log(req.body);
-    console.log(req.url);
-    console.log(req.query);
-    res.send("GET Successful!");
-    }
-    );
+const port = process.env.PORT || 3011;
 
-    app.get("/items/:vegetable", homeController.sendReqParam);
-    app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    }
-);
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+
+// Middleware to parse URL-encoded and JSON data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve static files from the public directory
+app.use(express.static("public"));
+
+// Log each request to the console
+app.use((req, res, next) => {
+    console.log(`Request made to: ${req.url}`);
+    next();
+});
+
+// Route to render index.ejs
+app.get("/", (req, res) => {
+    res.render("index"); // Render index.ejs from the views folder
+});
+
+// Example route with parameter handling
+app.get("/items/:vegetable", homeController.sendReqParam);
+
+// Route to render a view with a parameter
+
+app.get("/name/:myName", homeController.respondWithName);
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
